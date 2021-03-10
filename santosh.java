8ffe3e103dcafef0d00eb2be33c222e157a98d37
santosh725 <saisantosh515@gmail.com>
2021-03-09 19:10:21 -0500

    Revert "my second commit"
    This reverts commit c462e0e45f3d4e587a0d19f6d3c0413935e5907c.

--- santosh.java
+++ santosh.java
@@ -1,5 +1,3 @@
 this is being added before adding to working area santosh.java
 
 this is being added after adding to the staging area
-
-editing this file after pushing your updates to the remote repo
